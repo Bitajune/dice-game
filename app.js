95469abab5747eaf0135fc9f90543a49b739f7e6
Bitajune <beheshtibita@gmail.com>
2020-06-10 12:32:26 -0700

    toggle players when they roll a 1

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
@@ -22,15 +22,21 @@ document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     //This is an anonymous function. it does not have a name and cannot be used outside of this querySelector.
-    // 1. we need a random number
+    // 1. Get a random number
     let dice = Math.floor(Math.random() * 6) + 1;
     // 2. Display the result
     let diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
     // 3. Update the round score IF the rolled number was NOT a 1
-
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    }
 });
 
 
-// document.querySelector('#current-' + activePlayer).textContent = dice;
