ee902539002a926f669a08aa373077d50045fa5d
Bitajune <beheshtibita@gmail.com>
2020-06-12 10:19:34 -0700

    hold functionality and dry code

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
 let scores, roundScore, activePlayer;
 
 scores = [0, 0];
@@ -30,22 +19,35 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     diceDOM.src = 'dice-' + dice + '.png';
     // 3. Update the round score IF the rolled number was NOT a 1
     if (dice !== 1) {
-        //Add score
+        //Add score and switch player
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-        //Toggle the player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-        //Reset the current score to 0
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-        //Toggle HTML with actice player
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        //Take away dice after 1 is rolled
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 });
 
 
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    // Add current score to global score
+    scores[activePlayer] += roundScore;
+    // Update the UI 
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    // Check if player won the game 
+    // Next player
+    nextPlayer();
+});
+
+function nextPlayer() {
+    //Toggle the player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    //Reset the current score to 0
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    //Toggle HTML with actice player
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    //Take away dice after 1 is rolled
+    document.querySelector('.dice').style.display = 'none';
+};
